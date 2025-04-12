a93457ca35787ca7975f90e1d49bee911aea1179
quotebot-web <sreesha205@gmail.com>
2025-04-12 10:28:43 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,10 @@
+import time
+
+def main():
+    print("Bot is running...")
+    while True:
+        print("Checking for signal...")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
